ceeaa717f482b2a92ac6aa5492f1785297fe02b8
16023054 <16023054@myrp.edu.sg>
2018-07-23 11:43:11 +0800

    Done

--- app/src/main/java/sg/edu/rp/c346/demoactionbar/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoactionbar/MainActivity.java
@@ -1,7 +1,8 @@
 package sg.edu.rp.c346.demoactionbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
